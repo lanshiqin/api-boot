6ea925ee867f2178d4fa8070b1bf30371e35c45e
lanshiqin <lanshiqin@outlook.com>
2017-12-15 11:25:26 +0800

    添加http工具类

--- server/src/main/java/com/lanshiqin/apiboot/core/utils/HttpUtils.java
+++ server/src/main/java/com/lanshiqin/apiboot/core/utils/HttpUtils.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
  * Http工具类
  */
 public class HttpUtils {
+
     /**
      * 获取用户真实IP地址，不使用request.getRemoteAddr()的原因是有可能用户使用了代理软件方式避免真实IP地址,
      * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值
